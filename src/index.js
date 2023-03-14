f61bde329031ca4d80302373c6bdedaaf4ea4584
tekostyshyn <tekostyshyn@gmail.com>
2023-03-14 12:03:50 +0200

    bugs fixed

--- src/index.js
+++ src/index.js
@@ -13,51 +13,58 @@ const END_MESSAGE =
   "We're sorry, but you've reached the end of search results.";
 const LIMIT_PER_PAGE = 40;
 const lightbox = new SimpleLightbox('.gallery a');
-let page = 1;
+let page;
 let searchQuery = '';
 
 refs.searchForm.addEventListener('submit', onSearch);
 refs.loadMoreButton.addEventListener('click', onLoadMore);
 
-function onSearch(e) {
+async function onSearch(e) {
   e.preventDefault();
 
   const form = e.currentTarget;
   searchQuery = form.elements.searchQuery.value;
+  page = 1;
 
   if (searchQuery.trim()) {
     clearMarkup();
     refs.loadMoreButton.classList.add('hidden');
+    try {
+      const result = await fetchPhotos(searchQuery, 1);
 
-    fetchPhotos(searchQuery, 1)
-      .then(result => {
-        if (result.totalHits === 0) {
-          Notify.failure(FAILURE_MESSAGE);
-          return;
-        }
-        if (result.totalHits > LIMIT_PER_PAGE) {
-          refs.loadMoreButton.classList.remove('hidden');
-        }
-        renderMarkup(result);
-        lightbox.refresh();
-        Notify.info(`Hooray! We found ${result.totalHits} images.`);
-      })
-      .catch(error => console.log(error))
-      .finally(form.reset());
+      if (result.totalHits === 0) {
+        Notify.failure(FAILURE_MESSAGE);
+        return;
+      }
+
+      if (result.totalHits > LIMIT_PER_PAGE) {
+        refs.loadMoreButton.classList.remove('hidden');
+      }
+
+      renderMarkup(result);
+      lightbox.refresh();
+      Notify.info(`Hooray! We found ${result.totalHits} images.`);
+    } catch (error) {
+      console.log(error.message);
+    } finally {
+      form.reset();
+    }
   }
 }
 
-function onLoadMore() {
+async function onLoadMore() {
   page += 1;
-  fetchPhotos(searchQuery, page)
-    .then(result => {
-      renderMarkup(result);
-      lightbox.refresh();
-      smoothScroll();
-      if (result.hits.length < LIMIT_PER_PAGE) {
-        refs.loadMoreButton.classList.add('hidden');
-        Notify.info(END_MESSAGE);
-      }
-    })
-    .catch(error => console.log(error));
+
+  try {
+    const result = await fetchPhotos(searchQuery, page);
+    renderMarkup(result);
+    lightbox.refresh();
+    smoothScroll();
+    if (result.hits.length < LIMIT_PER_PAGE) {
+      refs.loadMoreButton.classList.add('hidden');
+      Notify.info(END_MESSAGE);
+    }
+  } catch (error) {
+    console.log(error.message);
+  }
 }
